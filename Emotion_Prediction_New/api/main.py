9c5493297134e80a45f77d3f63eead9ca0c24642
lushiya <it20085858@my.sliit.lk>
2023-09-04 14:12:47 +0530

    API files added

--- Emotion_Prediction_New/api/main.py
+++ Emotion_Prediction_New/api/main.py
@@ -20,9 +20,15 @@ app.add_middleware(
     allow_headers=["*"],
 )
  
-MODEL = tf.keras.models.load_model("../models/1")
+MODEL_BREED = tf.keras.models.load_model("../models/InceptionV3BreedModel.h5")
+MODEL_EMOTION = tf.keras.models.load_model("../models/dog_emotion.h5")
+MODEL_AGE = tf.keras.models.load_model("../models/dog_age.h5")
+MODEL_SKINDISEASE = tf.keras.models.load_model("../models/skinDiseases.h5")
 
-CLASS_NAMES = ["Angry","Happy","Relaxed","Sad"]
+CLASS_NAMES_EMOTION = ["Angry","Happy","Relaxed","Sad"]
+CLASS_NAMES_AGE = ['Adult', 'Senior', 'Young']
+CLASS_NAMES_SKINDISEASE = ["Alopecia","Folliculitis","Lumps and bumps","Rheum","Ringworm","Ticks","Vitiligo"]
+CLASS_NAMES_BREED = ["afghan_hound", "african_hunting_dog", "airedale", "basenji", "basset", "beagle", "bedlington_terrier", "bernese_mountain_dog", "black-and-tan_coonhound", "blenheim_spaniel", "bloodhound", "bluetick", "border_collie", "border_terrier", "borzoi", "boston_bull", "bouvier_des_flandres", "brabancon_griffon", "bull_mastiff", "cairn", "cardigan", "chesapeake_bay_retriever", "chow", "clumber", "cocker_spaniel", "collie", "curly-coated_retriever", "dhole", "dingo", "doberman", "english_foxhound", "english_setter", "entlebucher", "flat-coated_retriever", "german_shepherd", "german_short-haired_pointer", "golden_retriever", "gordon_setter", "great_dane", "great_pyrenees", "groenendael", "ibizan_hound", "irish_setter", "irish_terrier", "irish_water_spaniel", "irish_wolfhound", "japanese_spaniel", "keeshond", "kerry_blue_terrier", "komondor", "kuvasz", "labrador_retriever", "leonberg", "lhasa", "malamute", "malinois", "maltese_dog", "mexican_hairless", "miniature_pinscher", "miniature_schnauzer", "newfoundland", "norfolk_terrier", "norwegian_elkhound", "norwich_terrier", "old_english_sheepdog", "otterhound", "papillon", "pekinese", "pembroke", "pomeranian", "pug", "redbone", "rhodesian_ridgeback", "rottweiler", "saint_bernard", "saluki", "samoyed", "schipperke", "scotch_terrier", "scottish_deerhound", "sealyham_terrier", "shetland_sheepdog", "standard_poodle", "standard_schnauzer", "sussex_spaniel", "tibetan_mastiff", "tibetan_terrier", "toy_terrier", "vizsla", "weimaraner", "whippet", "wire-haired_fox_terrier", "yorkshire_terrier"]
 
 @app.get("/ping")
 async def ping():
@@ -32,16 +38,78 @@ def read_file_as_image(data) -> np.ndarray:
     image = np.array(Image.open(BytesIO(data)))
     return image
 
-@app.post("/predict")
+# Breed
+@app.post("/predict/breed")
 async def predict(
     file: UploadFile = File(...)
 ):
     image = read_file_as_image(await file.read())
     img_batch = np.expand_dims(image, 0)
     
-    predictions = MODEL.predict(img_batch)
+    predictions = MODEL_BREED.predict(img_batch)
 
-    predicted_class = CLASS_NAMES[np.argmax(predictions[0])]
+    predicted_class = CLASS_NAMES_BREED[np.argmax(predictions[0])]
+    confidence = np.max(predictions[0])
+    return {
+        'class': predicted_class,
+        'confidence': float(confidence)
+    }
+
+# Emotion
+@app.post("/predict/emotion")
+async def predict(
+    file: UploadFile = File(...)
+):
+    image = read_file_as_image(await file.read())
+    img_batch = np.expand_dims(image, 0)
+    
+    predictions = MODEL_EMOTION.predict(img_batch)
+
+    predicted_class = CLASS_NAMES_EMOTION[np.argmax(predictions[0])]
+    confidence = np.max(predictions[0])
+    return {
+        'class': predicted_class,
+        'confidence': float(confidence)
+    }
+
+# Age
+@app.post("/predict/age")
+async def predict(
+    file: UploadFile = File(...)
+):
+    image = read_file_as_image(await file.read())
+    img_batch = np.expand_dims(image, 0)
+    
+    predictions = MODEL_AGE.predict(img_batch)
+
+    predicted_class = CLASS_NAMES_AGE[np.argmax(predictions[0])]
+    confidence = np.max(predictions[0])
+
+    age = None  # Initialize age
+    if predicted_class == 'Adult':
+        age = '2 - 7 years'
+    elif predicted_class == 'Senior':
+        age = 'above 7 years'
+    elif predicted_class == 'Young':
+        age = '1 - 2 years'
+
+    return {
+        'class': predicted_class,
+        'age': age,
+        'confidence': float(confidence)
+    }
+
+# Skin disease
+@app.post("/predict/skindisease")
+async def predict(
+    file: UploadFile = File(...)
+):
+    image = read_file_as_image(await file.read())
+    img_batch = np.expand_dims(image, 0)
+    
+    predictions = MODEL_SKINDISEASE.predict(img_batch)
+
+    predicted_class = CLASS_NAMES_SKINDISEASE[np.argmax(predictions[0])]
     confidence = np.max(predictions[0])
     return {
         'class': predicted_class,
--- Emotion_Prediction_New/api/mainBreed.py
+++ Emotion_Prediction_New/api/mainBreed.py
@@ -20,10 +20,11 @@ app.add_middleware(
     allow_headers=["*"],
 )
  
-MODEL = tf.keras.models.load_model("../models/InceptionV3BreedModel.h5")
+MODEL_BREED = tf.keras.models.load_model("../models/InceptionV3BreedModel.h5")
+MODEL_EMOTION = tf.keras.models.load_model("../models/1")
 
-# CLASS_NAMES = ["Angry","Happy","Relaxed","Sad"]
-CLASS_NAMES = ["afghan_hound", "african_hunting_dog", "airedale", "basenji", "basset", "beagle", "bedlington_terrier", "bernese_mountain_dog", "black-and-tan_coonhound", "blenheim_spaniel", "bloodhound", "bluetick", "border_collie", "border_terrier", "borzoi", "boston_bull", "bouvier_des_flandres", "brabancon_griffon", "bull_mastiff", "cairn", "cardigan", "chesapeake_bay_retriever", "chow", "clumber", "cocker_spaniel", "collie", "curly-coated_retriever", "dhole", "dingo", "doberman", "english_foxhound", "english_setter", "entlebucher", "flat-coated_retriever", "german_shepherd", "german_short-haired_pointer", "golden_retriever", "gordon_setter", "great_dane", "great_pyrenees", "groenendael", "ibizan_hound", "irish_setter", "irish_terrier", "irish_water_spaniel", "irish_wolfhound", "japanese_spaniel", "keeshond", "kerry_blue_terrier", "komondor", "kuvasz", "labrador_retriever", "leonberg", "lhasa", "malamute", "malinois", "maltese_dog", "mexican_hairless", "miniature_pinscher", "miniature_schnauzer", "newfoundland", "norfolk_terrier", "norwegian_elkhound", "norwich_terrier", "old_english_sheepdog", "otterhound", "papillon", "pekinese", "pembroke", "pomeranian", "pug", "redbone", "rhodesian_ridgeback", "rottweiler", "saint_bernard", "saluki", "samoyed", "schipperke", "scotch_terrier", "scottish_deerhound", "sealyham_terrier", "shetland_sheepdog", "standard_poodle", "standard_schnauzer", "sussex_spaniel", "tibetan_mastiff", "tibetan_terrier", "toy_terrier", "vizsla", "weimaraner", "whippet", "wire-haired_fox_terrier", "yorkshire_terrier"]
+CLASS_NAMES_EMOTION = ["Angry","Happy","Relaxed","Sad"]
+CLASS_NAMES_BREED = ["afghan_hound", "african_hunting_dog", "airedale", "basenji", "basset", "beagle", "bedlington_terrier", "bernese_mountain_dog", "black-and-tan_coonhound", "blenheim_spaniel", "bloodhound", "bluetick", "border_collie", "border_terrier", "borzoi", "boston_bull", "bouvier_des_flandres", "brabancon_griffon", "bull_mastiff", "cairn", "cardigan", "chesapeake_bay_retriever", "chow", "clumber", "cocker_spaniel", "collie", "curly-coated_retriever", "dhole", "dingo", "doberman", "english_foxhound", "english_setter", "entlebucher", "flat-coated_retriever", "german_shepherd", "german_short-haired_pointer", "golden_retriever", "gordon_setter", "great_dane", "great_pyrenees", "groenendael", "ibizan_hound", "irish_setter", "irish_terrier", "irish_water_spaniel", "irish_wolfhound", "japanese_spaniel", "keeshond", "kerry_blue_terrier", "komondor", "kuvasz", "labrador_retriever", "leonberg", "lhasa", "malamute", "malinois", "maltese_dog", "mexican_hairless", "miniature_pinscher", "miniature_schnauzer", "newfoundland", "norfolk_terrier", "norwegian_elkhound", "norwich_terrier", "old_english_sheepdog", "otterhound", "papillon", "pekinese", "pembroke", "pomeranian", "pug", "redbone", "rhodesian_ridgeback", "rottweiler", "saint_bernard", "saluki", "samoyed", "schipperke", "scotch_terrier", "scottish_deerhound", "sealyham_terrier", "shetland_sheepdog", "standard_poodle", "standard_schnauzer", "sussex_spaniel", "tibetan_mastiff", "tibetan_terrier", "toy_terrier", "vizsla", "weimaraner", "whippet", "wire-haired_fox_terrier", "yorkshire_terrier"]
 
 @app.get("/ping")
 async def ping():
@@ -33,16 +34,32 @@ def read_file_as_image(data) -> np.ndarray:
     image = np.array(Image.open(BytesIO(data)))
     return image
 
-@app.post("/predict")
+@app.post("/predict/breed")
 async def predict(
     file: UploadFile = File(...)
 ):
     image = read_file_as_image(await file.read())
     img_batch = np.expand_dims(image, 0)
     
-    predictions = MODEL.predict(img_batch)
+    predictions = MODEL_BREED.predict(img_batch)
 
-    predicted_class = CLASS_NAMES[np.argmax(predictions[0])]
+    predicted_class = CLASS_NAMES_BREED[np.argmax(predictions[0])]
+    confidence = np.max(predictions[0])
+    return {
+        'class': predicted_class,
+        'confidence': float(confidence)
+    }
+
+@app.post("/predict/emotion")
+async def predict(
+    file: UploadFile = File(...)
+):
+    image = read_file_as_image(await file.read())
+    img_batch = np.expand_dims(image, 0)
+    
+    predictions = MODEL_EMOTION.predict(img_batch)
+
+    predicted_class = CLASS_NAMES_EMOTION[np.argmax(predictions[0])]
     confidence = np.max(predictions[0])
     return {
         'class': predicted_class,
